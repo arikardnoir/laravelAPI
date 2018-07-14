3e0deb741fbf83453ff85a39a0b3dff039807cbf
arikardnoir <aristo.lopes@gmail.com>
2018-07-13 23:01:52 -0300

    Deploy Heroku

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'laravelserverapi'),
+            'username' => env('DB_USERNAME', 'arikardnoir'),
+            'password' => env('DB_PASSWORD', '@Ingridlopes1'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_general_ci',
